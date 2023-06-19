d5b5e87eb87ee1c82aabcc9119f5d819037feb7d
ZvikaYafi <tzviyehuday@balink.net>
2023-06-19 20:00:49 +0300

    I improved the code

--- week-2/TodoProject/TodosProject/TodosProject/ViewController.swift
+++ week-2/TodoProject/TodosProject/TodosProject/ViewController.swift
@@ -1,19 +1,11 @@
-//
-//  ViewController.swift
-//  TodosProject
-//
-//  Created by Zvika-Yafi on 18/06/2023.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
